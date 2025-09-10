784fe790b40b661e50fcdc91eb08fb01d481b782
Gabriel <gsmainiere1901@gmail.com>
2025-09-09 23:33:30 -0300

    feat: adding movies validations

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -12,10 +12,19 @@ export class MovieService {
   }
 
   public list(): IMovie[] {
-    return this.movieRepository.findAll();
+    const movies = this.movieRepository.findAll();
+    if (!movies || movies.length === 0){
+      throw new Error(`movie not found in database.`);
+    }
+    return movies
   }
 
   public getById(id: number): IMovie | undefined {
+    const movie = this.movieRepository.findById(id)
+
+    if (!movie){
+      throw new Error(`movie with ${id} not found`);
+    }
     return this.movieRepository.findById(id);
   }
 
@@ -24,18 +33,30 @@ export class MovieService {
   }
 
   public update(id: number, movieData: Partial<Omit<IMovie, "id">>): IMovie | undefined {
+    const movie = this.movieRepository.findById(id)
+
+    if (!movie){
+      throw new Error(`movie with ${id} not found. Unable to update.`);
+    }
     return this.movieRepository.update(id, movieData);
   }
 
   public delete(id: number): void {
+    const movie = this.movieRepository.findById(id)
+
+    if (!movie){
+      throw new Error(`movie with ${id} not found. Unable to delete.`);
+    }
     this.movieRepository.delete(id);
   }
 
   public addActorToMovie(movieId: number, actorId: number): IMovie | undefined {
     const movie = this.movieRepository.findById(movieId);
     const actor = this.actorService.getById(actorId);
-    if (!movie || !actor) return undefined;
 
+    if (!movie || !actor){
+      throw new Error (`Movie or actor not found.`);
+    }
     if (!movie.actors.includes(actorId)) movie.actors.push(actorId);
     if (!actor.movies.includes(movieId)) actor.movies.push(movieId);
 
@@ -44,8 +65,9 @@ export class MovieService {
 
   public listActorsByMovie(movieId: number) {
     const movie = this.movieRepository.findById(movieId);
-    if (!movie) return undefined;
-
+    if (!movie){
+      throw new Error(`movie with ${movieId} not found.`);
+    }
     return movie.actors.map(actorId => this.actorService.getById(actorId)).filter(Boolean);
   }
 }
